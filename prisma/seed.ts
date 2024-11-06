f8e0b6ee0657fd85239133e24630353eaed6b71f
Zelda <zeldac@hawaii.edu>
2024-11-05 17:08:04 -1000

    Contacts finished in 37 minutes

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -45,7 +45,23 @@ async function main() {
       },
     });
   });
+  config.defaultContacts.forEach(async (contact, index) => {
+    console.log(`  Adding contact: ${contact.firstName} ${contact.lastName}`);
+    await prisma.contact.upsert({
+      where: { id: index },
+      update: {},
+      create: {
+        firstName: contact.firstName,
+        lastName: contact.lastName,
+        address: contact.address,
+        image: contact.image,
+        description: contact.description,
+        owner: contact.owner,
+      },
+    });
+  });
 }
+
 main()
   .then(() => prisma.$disconnect())
   .catch(async (e) => {
--- src/components/ContactCardAdmin.tsx
+++ src/components/ContactCardAdmin.tsx
@@ -1,10 +1,10 @@
 'use client';
 
-import { Contact } from '@/lib/validationSchemas';
+import { Contact } from '@prisma/client';
 import { Card, Image } from 'react-bootstrap';
 
 /* Renders a single row in the List Stuff table. See list/page.tsx. */
-const ContactCard = ({ contact }: { contact: Contact }) => (
+const ContactCardAdmin = ({ contact }: { contact: Contact }) => (
   <Card className="h-100">
     <Card.Header>
       <Image src={contact.image} width={75} />
@@ -16,11 +16,10 @@ const ContactCard = ({ contact }: { contact: Contact }) => (
       <Card.Subtitle>{contact.address}</Card.Subtitle>
     </Card.Header>
     <Card.Body>
-      <Card.Text>
-        {contact.description}
-      </Card.Text>
+      <Card.Text>{contact.description}</Card.Text>
+      <p className="blockquote-footer">{contact.owner}</p>
     </Card.Body>
   </Card>
 );
 
-export default ContactCard;
+export default ContactCardAdmin;
